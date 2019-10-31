95f0965b25ab2ceae8a76d53e8b46b9e0aabdbee
Naz <nazarethisaiah@hotmail.com>
2019-10-31 18:44:50 -0400

    Created label for exit which will add the summ accumulated from iteration through the string and storing it  into a register. That  register is then told to output the value in it and then li $v0, 10 ends the program.

--- project1.s
+++ project1.s
@@ -1,6 +1,7 @@
 .data
 myID: 
 	.word 4
+nextline: .asciiz "\n"
 .text
 main:
 
@@ -19,6 +20,15 @@ main:
 #	ble $t1, 90, uppercase
 #	ble $t1, 122, lowercase	
 
+EXIT:
+	la $a0, nextline
+	li $v0, 4
+	syscall
+	add $a0, $s7, $zero
+	li $v0, 1
+	syscall
+	li $v0, 10
+	syscall
 #.data
 
 #insert_into:
