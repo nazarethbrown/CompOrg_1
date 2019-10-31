06ab284e05c2e646602d2e481b9d21c4670cec83
Naz <nazarethisaiah@hotmail.com>
2019-10-31 19:05:25 -0400

    Crested the numbers function which will subtract 48 from the character in the register which has already been identified as a number. This will convert the integer from ASCII Decimal value into the character's actual value or designated value according to the assignment's given range.

--- project1.s
+++ project1.s
@@ -29,6 +29,14 @@ EXIT:
 	syscall
 	li $v0, 10
 	syscall
+numbers:
+	bge $t0, 11, EXIT
+	lb $t1, 0($s1)
+	sub $t1, $t1, 48
+	add $s7, $s7, $t1
+	addi $t0, $t0, 1
+	addi $s1, $s1
+	j numbers
 #.data
 
 #insert_into:
