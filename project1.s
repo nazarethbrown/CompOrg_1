a809113556ff5e8306d41d3ee35630080bd8acc3
Naz <nazarethisaiah@hotmail.com>
2019-10-31 03:11:23 -0400

    This commit includes making space for the input from the user and limiting the input to 11 characters.

--- project1.s
+++ project1.s
@@ -1,26 +1,36 @@
 .data
-
-insert_into:
+myID: 
 	.word 4
-Ask_Input:
-	.asciiz "\Please Enter a String\n"
-Tell_Output:
-	.asciiz "\You Typed: \n"
 .text
-
 main:
-	la $a0, Ask_Input
-	li $v0, 4
-	syscall 
-	la $a0, insert_into
-	la $a1, insert_into
-	li $v0, 8 
-	syscall 
-	la $a0, Tell_Output 	
-	li $v0, 4 
-	syscall 
-	la $a0, insert_into 
-	li $v0, 4 
-	syscall 
-	li $v0, 10 
-	syscall 
+
+	li $v0, 8 #load immediate  opcode 8 at register $v0  
+	la $a3, myID #load address of myID into register $a
+	li $a0,11 #ensures input only accepts 11 characters
+	syscall #run the last three lines to get inout from user
+#.data
+
+#insert_into:
+#	.word 4
+#Ask_Input:
+#	.asciiz "\nPlease Enter ID Number: \n"
+#Tell_Output:
+#	.asciiz "\You Typed: \n"
+#.text
+
+#main:
+#	la $a0, Ask_Input
+#	li $v0, 4
+#	syscall 
+#	la $a0, insert_into
+#	la $a1, insert_into
+#	li $v0, 8 
+#	syscall 
+#	la $a0, Tell_Output 	
+#	li $v0, 4 
+#	syscall 
+#	la $a0, insert_into 
+#	li $v0, 4 
+#	syscall 
+#	li $v0, 10 
+#	syscall 
